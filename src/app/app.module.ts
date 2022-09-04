09139013fefbc95db8c9f8bc250215b4a1c166dc
Larissa Luana Leal <larissaluanaleal@hotmail.com>
2022-09-04 12:28:28 -0300

    config local id e tipo moeda p/ usar pipe currency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule,} from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule,} from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import ptBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(ptBr)
 
 import { MaterialModule } from './material/material.module';
 import { FinancialModule } from './financial/financial.module';
-
+import { AppComponent } from './app.component';
 
 
 @NgModule({
@@ -26,7 +28,11 @@ import { FinancialModule } from './financial/financial.module';
     FinancialModule
   ],
   exports:[MaterialModule],
-  providers: [],
+  providers: [
+  { provide: LOCALE_ID, useValue: 'pt' },
+  { provide:  DEFAULT_CURRENCY_CODE, useValue: 'BRL'}
+],
   bootstrap: [AppComponent],
+
 })
 export class AppModule { }
